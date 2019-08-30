57f552a50bb16cd0ee56481779cd6ce141e2ae3c
andy <andy@a1013c61-bf79-4f7f-8793-d59588655a1e>
2019-08-30 00:17:40 +0000

    add sound stuff

--- clockweb.py
+++ clockweb.py
@@ -2,6 +2,8 @@ from flask import Flask,request,redirect
 import random
 import sys
 import os
+import subprocess
+import re
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,20 +11,76 @@ app = Flask(__name__)
 def page():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as text_file:
         temp = int(text_file.read())
+        v1=subprocess.check_output(['amixer', 'get', 'PCM'])
+        v2=re.search(r'([0-9]+)%', v1)
+        if v2:
+            curvolume=v2.group(1)
+        else:
+            curvolume="unknown"
 
     return (
 '''<html>
 <head><title>Matthew's Clock</title></head>
 <body>
+<table>
+<tr>
 <form action = "/post" method = "POST">
-<p>
+<td>
 <input type = "submit" value = "Send Message:" />
+</td>
+<td>
 <input type = "text" name = "msg" />
-</p>
+</td>
 </form>
+</tr>
+<tr>
+<form action = "/speak" method = "POST">
+<td>
+<input type = "submit" value = "Speak Message:" />
+</td>
+<td>
+<input type = "text" name = "msg" />
+</td>
+</form>
+</tr>
+<tr>
 <form action = "/joke" method = "POST">
-<p><input type = "submit" value = "Tell me a joke" /></p>
+<td>
+<input type = "submit" value = "Tell me a joke" />
+</td>
+<td/>
 </form>
+</tr>
+<tr>
+<form action = "/jokeread" method = "POST">
+<td>
+<input type = "submit" value = "Read me a joke" />
+</td>
+<td/>
+</form>
+</tr>
+</table>
+<hr/>
+<table border="1">
+<tr>
+<td width="80"><a href="/volume?level=0">Mute</a></td>
+<td width="80"><a href="/volume?level=1">Lowest</a></td>
+<td width="80"><a href="/volume?level=10">1</a></td>
+<td width="80"><a href="/volume?level=20">2</a></td>
+<td width="80"><a href="/volume?level=30">3</a></td>
+<td width="80"><a href="/volume?level=40">4</a></td>
+<td width="80"><a href="/volume?level=50">Max</a></td>
+</tr>
+<tr>
+<td>Volume:<br/>''' + curvolume + '''</td>
+<td><a href="/radio?name=stop">Stop Radio</a></td>
+<td><a href="/radio?name=jackfm">Jack FM</a></td>
+<td><a href="/radio?name=radio1">BBC Radio 1</a></td>
+<td><a href="/radio?name=radio2">BBC Radio 2</a></td>
+<td><a href="/radio?name=radio3">BBC Radio 3</a></td>
+<td><a href="/radio?name=radio4">BBC Radio 4</a></td>
+</tr>
+</table>
 <hr/>
 <table border="1">
 <tr>
@@ -45,6 +103,13 @@ def result():
             text_file.write(request.form['msg'])
     return redirect("/", code=302)
 
+@app.route('/speak',methods = ['POST'])
+def speak():
+    if request.method == 'POST':
+        #print request.form['msg']
+        subprocess.check_call(['/home/pi/audio.sh', 'say', msg])
+    return redirect("/", code=302)
+
 @app.route('/joke',methods = ['POST','GET'])
 def joke():
     joke = random.randint(1,numjokes)
@@ -53,6 +118,16 @@ def joke():
     #print >> sys.stderr, ("Joke %d of %d: %s\n" % (joke,numjokes,msg))
     with open(("/run/clockmsg/%d" % random.randint(1,99999999)), "w") as text_file:
         text_file.write(msg)
+    return redirect("/", code=302)
+
+@app.route('/jokeread',methods = ['POST','GET'])
+def jokeread():
+    joke = random.randint(1,numjokes)
+    msg=jokes[joke]
+
+    #print >> sys.stderr, ("Joke %d of %d: %s\n" % (joke,numjokes,msg))
+    subprocess.check_call(['/home/pi/audio.sh', 'say', msg])
+
     return redirect("/", code=302)
 
 @app.route('/manage',methods = ['GET'])
@@ -66,6 +141,30 @@ def manage():
             os.system("echo 'sleep 5 ; /sbin/shutdown -r now' | at now")
         elif (a == "shutdown"):
             os.system("echo 'sleep 5 ; /sbin/shutdown -h now' | at now")
+
+    return redirect("/", code=302)
+
+@app.route('/volume',methods = ['GET'])
+def volume():
+    if request.method == 'GET':
+        print >>sys.stderr, request.args.get("level")
+        l = int(request.args.get("level"))
+        if ((l >= 0) and (l <= 51)):
+            subprocess.check_call(['amixer', 'cset', 'numid=1', ('%d%%' % l)])
+        else:
+            print >>sys.stderr, ("Bad volume %d\n" % l)
+
+    return redirect("/", code=302)
+
+@app.route('/radio',methods = ['GET'])
+def radio():
+    if request.method == 'GET':
+        print >>sys.stderr, request.args.get("action")
+        r = request.args.get("name")
+        if (r == "stop"):
+            subprocess.check_call(['/home/pi/audio.sh', 'stop'])
+        else:
+            subprocess.check_call(['/home/pi/audio.sh', 'radio', r])
 
     return redirect("/", code=302)
 
