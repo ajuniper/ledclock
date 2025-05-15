e97093857feab95ceb53697065ebcd353708e3b1
andy <andy@a1013c61-bf79-4f7f-8793-d59588655a1e>
2025-05-15 21:54:20 +0000

    retry on failed transmit

--- sunrise.sh
+++ sunrise.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-pin=23
+pin=4
 gap=120
 # p1=buttons
 function tx ()
 {
-	sudo /home/pi/irsling -p $pin -f /home/pi/jedi_new.conf -- $1
+	while ! sudo timeout 3 /home/pi/irsling -p $pin -f /home/pi/jedi_new.conf -- $1 ; do
+            sleep 3
+        done
 	sleep ${2:-$gap}
+	#sleep 5
 }
 
-tx "1 yellow - - - -"
-tx +
-tx +
-tx +
-tx +
-tx white 300
-tx "0 0 0"
+if [[ $1 = start ]] ; then
+    tx "1 1 yellow - - - -"
+    tx +
+    tx +
+    tx +
+    tx +
+    tx white 1
+else
+    tx "0 0 0"
+fi
